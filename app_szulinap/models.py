ce1aaa092fb8f4d72473788a5263f7f524199bb7
eigner_krisztian <eigner_krisztian.22f@szlgbp.hu>
2025-05-23 11:46:23 +0200

    asdgsdf

--- app_szulinap/models.py
+++ app_szulinap/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Ember(models.Model):
+    teljesnev = models.CharField(_max_length=255)
+    becenev = models.CharField(_max_length=255)
+    szulinap = models.DateField()
+    nevnap = models.DateField()
+    ajandek = models.CharField(max_length=255)
+    
+
+    class Meta:
+        verbose_name = "Ember"
+        verbose_name_plural = "Emberek"
+
+    def __str__(self):
+        return self.becenev, self.szulinap, self.nevnap
+
+    def letrehozas_sor_alapjan(sor):
+        sor = sor.strip()
+        if not sor:
+            return
+        sor = sor.split('\t')
+        ember, is_created= Ember.objects.get_or_create(teljesnev = sor[0], becenev = sor[1], szulinap = sor[2], nevnap = sor[3], ajandek = [4])
+
+        ember.save()
+        return ember
--- app_szulinap/views.py
+++ app_szulinap/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -11,3 +13,20 @@ def fooldal(request):
 
 def ujember(request):
     return render(request, 'app_szulinap/ujember.html')
+
+
+def feldolgoz(request):
+    if request.method != 'POST':
+        return HttpResponse('Ez nem post request')
+    if 'szoveg' not in request.POST.keys():
+        return HttpResponse('nem jött post request')
+    
+
+    sorok = request.POST['szoveg'].split('\n')[1:]
+
+    for sor in sorok:
+        Ember.letrehozas_sor_alapjan(sor)
+
+    print('Beolvasás vége')
+    
+    return render (request, 'app_szulinap/kuld.html')
